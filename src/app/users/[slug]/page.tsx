03a367fdd22e25b22db24429068a6d64aa5b1a4b
Emin Metin <eminsteam58@hotmail.com>
2024-04-06 20:35:51 +0300

    task intern commit

--- src/app/users/[slug]/page.tsx
+++ src/app/users/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Users from '../../../../users.json';
 import UserCard from '@/app/components/UserCard';
 
 export default function User({ params }: { params: UserProfile }) {
+  console.log(slug);
   const slug = params.slug;
   const filteredUser = Users.find((user) => user.slug === slug);
   return <UserCard user={filteredUser} pagetype={``} />;
